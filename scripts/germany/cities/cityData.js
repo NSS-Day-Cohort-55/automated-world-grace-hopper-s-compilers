a9792738bca85b581f22346935afd31e8ed9c9cc
wes-mitchell <wmdrums@gmail.com>
2022-03-02 16:47:24 -0600

    adds data to cityData.js famousCitizensData. and ladmarksData.js for Germany page

---
+++ scripts/germany/cities/cityData.js
@@ -0,0 +1,26 @@
+const cities = [
+  {
+    name: 'Berlin',
+    population: '3.6 Million',
+    description: 'Germany\'s capital that dates back to the 13 century',
+    url: <a href="https://en.wikipedia.org/wiki/Berlin" target="_blank">Berlin Wiki</a>
+  },
+  {
+    name: 'Munich',
+    population: '1.4 Million',
+    description: 'Munich is the capital of Bavaria and is know best for it\'s annual Oktoberfest celebration.',
+    url: <a href="https://en.wikipedia.org/wiki/Munich" target="_blank">Munich Wiki</a>
+  },
+  {
+    name: 'Hamburg',
+    population: '1.8 Million',
+    description: 'Hamburg is a major port city in Germany that is connected to the North Sea by the Elbe River.',
+    url: <a href="https://en.wikipedia.org/wiki/Hamburg" target="_blank">Hamburg Wiki</a>
+  },
+  {
+    name: 'Frankfurt',
+    population: '753 Thousand',
+    description: 'Frankfurt is a central German city and considered a major hub that is home to the European Central Bank.',
+    url: <a href="https://en.wikipedia.org/wiki/Frankfurt" target="_blank">Frankfurt Wiki</a>
+  }
+]
---
+++ scripts/germany/famousCitizens/famousCitizensData.js
@@ -0,0 +1,26 @@
+const famousCitizens = [
+  {
+    name: 'Albert Einstein',
+    occupation: 'Physicist',
+    description: 'Einstein was a theoretical physicist and is best known for developing the theory of relativity',
+    url: <a href="https://en.wikipedia.org/wiki/Albert_Einstein" target="_blank">Albert Einstein Wiki</a>
+  },
+  {
+    name: 'Angela Merkel',
+    occupation: 'Chancellor of Germany',
+    description: 'Merkel is a retired politician and scientist who served as the chancellor of Germany from 2005 to 2021',
+    url: <a href="https://en.wikipedia.org/wiki/Angela_Merkel" target="_blank">Angela Merkel Wiki</a>
+  },
+  {
+    name: 'Ludwig van Beethoven',
+    occupation: 'Composer and Pianist',
+    description: 'Beethoven\'s works rank amongst the most performed of the classical music repertoire and span the transition from the Classical period to the Romantic era in classical music',
+    url: <a href="https://en.wikipedia.org/wiki/Ludwig_van_Beethoven" target="_blank">Ludwing van Beethoven Wiki</a>
+  },
+  {
+    name: 'Johann Sebastian Bach',
+    occupation: 'Composer',
+    description: 'Bach was a composer in the late Baroque period known for his orchestral music such as the Brandenburg Concertos',
+    url: <a href="https://en.wikipedia.org/wiki/Johann_Sebastian_Bach" target="_blank">Johann Sebastian Bach Wiki</a>
+  }
+]
