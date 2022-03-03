fca63d39178a47f8a9a30f6477ae6a2adbebc1a7
wes-mitchell <wmdrums@gmail.com>
2022-03-03 16:02:40 -0600

    fills out germany index page with images and styling

--- scripts/germany/landmarks/landmark.js
+++ scripts/germany/landmarks/landmark.js
@@ -1,16 +1,16 @@
-
+import { getLandmarks } from "./landmarkData.js"
 
 export const landmark = (landmarkObj) => {
-  return `<article class="landmark-card">
-      <ul>
-        <li class="landmark-details">Name: ${landmarkObj.name}</li>
-        <li class="landmark-details">Occupation: ${landmarkObj.year}</li>
-        <li class="landmark-details">Description: ${landmarkObj.description}</li>
-        <li class="landmark-details"><a href="${landmarkObj.url}" target="_blank">Click here for more</a></li>
-      </ul>
-        </article>`
+    return `<article class="landmark-card"> 
+				<ul>
+					<li class="landmark-details">Name: ${landmarkObj.name}</li>
+					<li class="landmark-details">Occupation: ${landmarkObj.year}</li>
+					<li class="landmark-details">Description: ${landmarkObj.description}</li>
+					<li class="landmark-details"><a href="${landmarkObj.url}" target="_blank">Click here for more</a></li>
+				</ul>
+        	</article>`
 }
 
 export const numberOfLandmarks = () => {
-
+    return `<h3>The Top ${getLandmarks().length} Landmarks To Visit</h3>`
 }
