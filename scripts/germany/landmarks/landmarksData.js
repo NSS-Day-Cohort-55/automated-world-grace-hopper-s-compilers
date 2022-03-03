d3690e9f85e3d0d766a4aee92a019e2789af01e6
wes-mitchell <wmdrums@gmail.com>
2022-03-03 10:37:04 -0600

    removes anchor tags from url value in Germany and adds germany images to folder

--- scripts/germany/landmarks/landmarksData.js
+++ scripts/germany/landmarks/landmarksData.js
@@ -3,24 +3,24 @@ const landmarkData = [
     name: 'Berlin Wall',
     year: '1961 - 1989',
     description: 'The Berlin Wall was a guarded concrete barrier that physically and ideologically divided Berlin from 1961 to 1989.',
-    url: <a href="https://en.wikipedia.org/wiki/Berlin_Wall" target="_blank">Berlin Wall</a>
+    url: 'https://en.wikipedia.org/wiki/Berlin_Wall'
   },
   {
     name: 'Brandenburg Gate',
     year: '1791',
     description: 'The Brandenburg Gate is an 18th-century neoclassical monument in Berlin.',
-    url: <a href="https://en.wikipedia.org/wiki/Brandenburg_Gate" target="_blank">Brandenburg Gate</a>
+    url: 'https://en.wikipedia.org/wiki/Brandenburg_Gate'
   },
   {
     name: 'Mount Zugspitze',
     year: 'First ascent on Aug 27th, 1820',
     description: 'Mount Zugspitze is the highest peak of the Wetterstein Mountains as well as the highest mountain in Germany.',
-    url: <a href="https://en.wikipedia.org/wiki/Zugspitze" target="_blank">Mount Zugspitze</a>
+    url: 'https://en.wikipedia.org/wiki/Zugspitze'
   },
   {
     name: 'Eltz Castle',
     year: '1157',
     description: 'Eltz Castle is a medieval castle nestled in the hills above the Moselle between Koblenz and Trier, Germany.',
-    url: <a href="https://en.wikipedia.org/wiki/Eltz_Castle" target="_blank">Eltz Castle</a>
+    url: 'https://en.wikipedia.org/wiki/Eltz_Castle'
   }
 ]
