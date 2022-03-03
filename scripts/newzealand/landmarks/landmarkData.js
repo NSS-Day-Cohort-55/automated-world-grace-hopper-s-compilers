994e6622148cf8993b290ea696bb8c7bf7840078
LeeRJennings <leerjennings@outlook.com>
2022-03-03 08:41:26 -0600

    added data for New Zealand

---
+++ scripts/newzealand/landmarks/landmarkData.js
@@ -0,0 +1,26 @@
+const landmarkData = [
+    {
+      name: "Moeraki Boulders",
+      year: "approx. 60 million years old",
+      description: "The Moeraki Boulders are unusually large spherical boulders lying along a stretch of Koekohe Beach on the wave-cut Otago coast of New Zealand between Moeraki and Hampden.",
+      url: "https://en.wikipedia.org/wiki/Moeraki_Boulders"
+    },
+    {
+      name: "Craters of the Moon",
+      year: "Discovered in 1859",
+      description: "Craters of the Moon Thermal Area is a region with geothermal activity north of Taupo, New Zealand.",
+      url: "https://en.wikipedia.org/wiki/Craters_of_the_Moon_(geothermal_site)"
+    },
+    {
+      name: "The Sky Tower",
+      year: "1997",
+      description: "The Sky Tower is a telecommunications and observation tower in Auckland, New Zealand.",
+      url: "https://en.wikipedia.org/wiki/Sky_Tower_(Auckland)"
+    },
+    {
+      name: "Hobbiton Movie Set",
+      year: "1999",
+      description: "The Hobbiton Movie Set was a significant location used for The Lord of the Rings film trilogy and The Hobbit film trilogy.",
+      url: "https://en.wikipedia.org/wiki/Hobbiton_Movie_Set"
+    }
+  ]
