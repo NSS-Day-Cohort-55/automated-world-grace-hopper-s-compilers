5b725e60fadf949525d65561b0bc3a9f30c45c8e
Siprachanh <sipra.chant@gmail.com>
2022-03-03 23:08:23 -0600

    Australia JS edits and images added

--- scripts/australia/famousCitizens/famousCitizenList.js
+++ scripts/australia/famousCitizens/famousCitizenList.js
@@ -1,51 +1,24 @@
-/**
- *  famousCitizenList renders individual famousCitizen objects as HTML
- */
+
  import { getfamousCitizens, getfamousCitizenNames } from "./famousCitizenData.js"
  import { famousCitizen, famousCitizenNames } from "./famousCitizen.js"
  
  export const famousCitizenList = () => {
  
-     // Get a reference to the `<section class="famousCitizenList">` element
+    
      const contentElement = document.querySelector(".famousCitizenList")
      const famousCitizens = getfamousCitizens()
  
-     // make sure we provided a valid html selector above
+    
      if (contentElement) {
  
-         // Declare a variable to hold on to the famousCitizen HTML representation
          let famousCitizenHTMLRepresentation = "";
  
-         // loop over the array of famousCitizen
+         
          for (const eachfamousCitizen of famousCitizens) {
              console.log(eachfamousCitizen);
              famousCitizenHTMLRepresentation += `${famousCitizen(eachfamousCitizen)}`    
          }
-         // Set the dom equal to the variable containing the famousCitizen html representations
-         // Add to the existing HTML in the content element
+         
          contentElement.innerHTML += `${famousCitizenHTMLRepresentation}`
      }
  }
-
-//  export const famousCitizenNamesList = () => {
- 
-//     // Get a reference to the `<section class="famousCitizenList">` element
-//     const contentElement = document.querySelector(".famousCitizenNamesList")
-//     const names = getfamousCitizenNames()
-
-//     // make sure we provided a valid html selector above
-//     if (contentElement) {
-
-//         // Declare a variable to hold on to the famousCitizen HTML representation
-//         let nameHTMLRepresentation = "";
-
-//         // loop over the array of famousCitizen
-//         for (const name of names) {
-//             console.log(name);
-//             nameHTMLRepresentation += `${famousCitizenNames(name)}`    
-//         }
-//         // Set the dom equal to the variable containing the famousCitizen html representations
-//         // Add to the existing HTML in the content element
-//         contentElement.innerHTML += `${nameHTMLRepresentation}`
-//     }
-// }
--- scripts/australia/landmarks/landmark.js
+++ scripts/australia/landmarks/landmark.js
@@ -4,7 +4,7 @@ export const landmark = (landmarkObj) => {
     return `<article class="landmark-card">
 				<ul>
 					<li class="landmark-details">Name: ${landmarkObj.name}</li>
-					<li class="landmark-details">Occupation: ${landmarkObj.year}</li>
+					<li class="landmark-details">Year: ${landmarkObj.year}</li>
 					<li class="landmark-details">Description: ${landmarkObj.description}</li>
 					<li class="landmark-details"><a href="${landmarkObj.url}" target="_blank">Click here for more</a></li>
 				</ul>
